f4b5d6c8b5e32748bd356172736721cbeaeb3d6b
John Sundell <john.sundell@me.com>
2015-06-16 00:18:16 +0200

    Document CGRect+SwiftKit

--- Source/CGRect+SwiftKit.swift
+++ Source/CGRect+SwiftKit.swift
@@ -1,6 +1,8 @@
 import CoreGraphics
 
+/// Swift extensions to CGRect
 public extension CGRect {
+    /// The rectangle stripped of any origin information, marking its bounds
     var bounds: CGRect {
         var bounds = self
         bounds.origin = CGPoint()
