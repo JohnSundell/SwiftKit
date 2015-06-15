1b69a0c44adaec5b71691ce17c05763302d3abb5
John Sundell <john.sundell@me.com>
2015-06-15 23:57:55 +0200

    Add Identifier + tests

---
+++ Source/Identifier.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+/// A structure that can be used as a value-type replacement for NSUUID
+public struct Identifier: Hashable {
+    public var hashValue: Int { return self.identifierString.hashValue }
+    public let identifierString: String
+    
+    public init() {
+        self.identifierString = NSUUID().UUIDString
+    }
+}
+
+/// Equatable support for Identifier
+public func ==(lhs: Identifier, rhs: Identifier) -> Bool {
+    return lhs.identifierString == rhs.identifierString
+}
