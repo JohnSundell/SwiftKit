e06f937d80f2ae6e6ece0197dc6d7950aae15485
John Sundell <john.sundell@me.com>
2015-07-21 23:20:05 +0200

    Add Serializable

---
+++ Source/Serializable.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+/// Protocol adopted by objects that are representable by a serialized dictionary
+public protocol Serializable {
+    /// Serialize the object into a dictionary
+    func serialize() -> UnboxableDictionary
+}
