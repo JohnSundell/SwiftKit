45b3c75a18499d9de4872bf1174dd0646ce99569
John Sundell <john.sundell@me.com>
2015-07-22 23:07:56 +0200

    Add String+SwiftKit

---
+++ Source/String+SwiftKit.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+/// SwiftKit extensions to the standard library String
+public extension String {
+    /// The length of the string as an Integer
+    var length: Int { return count(self) }
+}
