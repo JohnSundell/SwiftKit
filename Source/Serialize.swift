80881f00050b20325e2568203ba4b3a717b95617
John Sundell <john.sundell@me.com>
2015-07-22 02:21:44 +0200

    Add Serialize()

---
+++ Source/Serialize.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// Serialize an array of objects that conform to Serializable
+public func Serialize<T: Serializable>(array: [T]) -> [UnboxableDictionary] {
+    return array.map() {
+        return $0.serialize()
+    }
+}
