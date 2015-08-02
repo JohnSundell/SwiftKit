086df31bf3b7299ed337501b4729c52ab8b7472e
John Sundell <john.sundell@me.com>
2015-08-02 02:17:22 +0200

    Animate: Add more overloads with completion handlers

--- Source/iOS/Animate.swift
+++ Source/iOS/Animate.swift
@@ -1,15 +1,23 @@
-#if os(iOS)
-
 import UIKit
+    
+/// Perform a closure of animations with a certain duration, optionally attaching a completion handler
+public func Animate(duration: NSTimeInterval, animations: () -> Void, onCompletion: (() -> Void)?) {
+    UIView.animateWithDuration(duration, animations: animations) { didComplete -> Void in
+        onCompletion?()
+    }
+}
 
 /// Perform a closure of animations with a certain duration
 public func Animate(duration: NSTimeInterval, animations: () -> Void) {
-    UIView.animateWithDuration(duration, animations: animations)
-}
- 
-/// Perform a closure of animations with a default 0.3 second duration
-public func Animate(animations: () -> Void) {
-    Animate(0.3, animations)
+    Animate(duration, animations, nil)
 }
 
-#endif
+/// Perform a closure of animations with a default 0.3 second duration, optionally attaching a completion handler
+public func Animate(animations: () -> Void, onCompletion: (() -> Void)?) {
+    Animate(0.3, animations, onCompletion)
+}
+
+/// Perform a closure of animations with a default 0.3 second duration
+public func Animate(animations: () -> Void) {
+    Animate(animations, nil)
+}
