afe5fd805e32a1fdf9ea3bae5113d70d2eda1d91
John Sundell <john.sundell@me.com>
2015-08-10 23:18:42 +0200

    Unwrap: Use @autoclosure to avoid unnecessary computation

--- Source/Unwrap.swift
+++ Source/Unwrap.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 /// Attempt to unwrap an optional, and if it's nil - return a default value
-public func Unwrap<T>(variable: T?, defaultValue: T) -> T {
+public func Unwrap<T>(variable: T?, @autoclosure defaultValue: () -> T) -> T {
     if let unwrapped = variable {
         return unwrapped
     }
     
-    return defaultValue
+    return defaultValue()
 }
