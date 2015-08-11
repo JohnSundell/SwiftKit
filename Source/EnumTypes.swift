38d735fdc4568a945b8452e2e738bb14f08debd5
John Sundell <john.sundell@me.com>
2015-08-12 01:36:45 +0200

    Remove now obsolete EnumTypes

--- Source/EnumTypes.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// Protocol that can be used as a constraint for Enums that inherit from `Int`
-public protocol IntEnum {
-    init?(rawValue: Int)
-    var rawValue: Int { get }
-}
