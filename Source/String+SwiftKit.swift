7b580b8060ea1d158217df66cdf5f321c8922369
John Sundell <john.sundell@me.com>
2015-08-24 01:41:08 +0200

    String+SwiftKit: Make length public

--- Source/String+SwiftKit.swift
+++ Source/String+SwiftKit.swift
@@ -3,5 +3,5 @@ import Foundation
 /// SwiftKit extensions to the standard library String struct
 public extension String {
     /// The length of the string as an Int
-    var length: Int { return self.characters.count }
+    public var length: Int { return self.characters.count }
 }
