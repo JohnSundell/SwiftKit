730742b65b388fd19bbf424961fc449e05d7bc2c
John Sundell <john.sundell@me.com>
2015-08-24 01:40:56 +0200

    Calculate: Add @noescape attribute

--- Source/Calculate.swift
+++ Source/Calculate.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 /// Function that can be used to encapsulate code using closures
-public func Calculate<T>(closure: () -> T) -> T {
+public func Calculate<T>(@noescape closure: () -> T) -> T {
     return closure()
 }
