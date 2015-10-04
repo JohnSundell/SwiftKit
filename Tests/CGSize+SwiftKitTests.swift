d2857baed21eb018fade78580b57b061e9e008f6
John Sundell <john.sundell@me.com>
2015-10-04 18:45:18 +0200

    CGSize tests: Refactor into constants to remove warnings

--- Tests/CGSize+SwiftKitTests.swift
+++ Tests/CGSize+SwiftKitTests.swift
@@ -3,29 +3,25 @@ import CoreGraphics
 
 class CGSizeSwiftKitTests: XCTestCase {
     func testScalingWithPositiveScale() {
-        var size = CGSize(width: 10, height: 5)
-        var scaledSize = size.sizeWithScale(2)
-        
+        let scaledSize = CGSize(width: 10, height: 5).sizeWithScale(2)
         XCTAssertTrue(CGSize(width: 20, height: 10) == scaledSize)
     }
     
     func testScalingWithNegativeScale() {
-        var size = CGSize(width: 10, height: 5)
-        var scaledSize = size.sizeWithScale(-3)
-        
+        let scaledSize = CGSize(width: 10, height: 5).sizeWithScale(-3)
         XCTAssertTrue(CGSize(width: -30, height: -15) == scaledSize)
     }
     
     func testCenterPointInLargerSize() {
-        var size = CGSize(width: 10, height: 20)
-        var largerSize = CGSize(width: 50, height: 100)
+        let size = CGSize(width: 10, height: 20)
+        let largerSize = CGSize(width: 50, height: 100)
         
         XCTAssertTrue(CGPoint(x: 20, y: 40) == size.centerPointInSize(largerSize))
     }
     
     func testCenterPointInSmallerSize() {
-        var size = CGSize(width: 10, height: 20)
-        var smallerSize = CGSize(width: 6, height: 2)
+        let size = CGSize(width: 10, height: 20)
+        let smallerSize = CGSize(width: 6, height: 2)
         
         XCTAssertTrue(CGPoint(x: -2, y: -9) == size.centerPointInSize(smallerSize))
     }
