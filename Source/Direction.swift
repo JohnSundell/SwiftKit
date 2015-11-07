15910bb2384058a92d7c0639fe72db23f1f3e589
John Sundell <john.sundell@me.com>
2015-11-07 15:06:39 +0100

    Add Direction (with both 4 & 8 way variants)

---
+++ Source/Direction.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+/// Structure acting as a namespace for enums describing directions
+public struct Direction {
+    /// An enum that describes a direction in any of 4 ways
+    public enum FourWay: Int {
+        case Up
+        case Right
+        case Down
+        case Left
+    }
+    
+    /// An ennum that describes a direction in any of 8 ways
+    public enum EightWay: Int {
+        case Up
+        case UpRight
+        case Right
+        case RightDown
+        case Down
+        case DownLeft
+        case Left
+        case LeftUp
+    }
+}
