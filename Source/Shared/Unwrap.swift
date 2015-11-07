7b4efeaa799f25caf594af8bc4ec2543a027217e
John Sundell <john.sundell@me.com>
2015-11-07 17:32:07 +0100

    Add Unwrap(or:) to be able to run a closure

--- Source/Shared/Unwrap.swift
+++ Source/Shared/Unwrap.swift
@@ -2,9 +2,10 @@ import Foundation
 
 /// Attempt to unwrap an optional, and if it's nil - return a default value
 public func Unwrap<T>(variable: T?, @autoclosure defaultValue: () -> T) -> T {
-    if let unwrapped = variable {
-        return unwrapped
-    }
-    
-    return defaultValue()
+    return variable ?? defaultValue()
+}
+
+/// Attempt to unwrap an optional, and if it's nil - return a value from a closure
+public func Unwrap<T>(variable: T?, or defaultValueClosure: () -> T) -> T {
+    return variable ?? defaultValueClosure()
 }
