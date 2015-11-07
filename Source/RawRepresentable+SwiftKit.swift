0d20d184083da9d379bed5440af9bc1cb197f907
John Sundell <john.sundell@me.com>
2015-11-07 17:13:10 +0100

    Add RawRepresentable+SwiftKit

---
+++ Source/RawRepresentable+SwiftKit.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/// Extension that enables enums with a numeric raw value to be strideable
+public extension RawRepresentable where RawValue: Number {
+    /// Go to the next member of the enum
+    public func next() -> Self? {
+        return Self(rawValue: RawValue(self.rawValue.toDouble() + 1))
+    }
+    
+    /// Go to the member that corresponds to the current raw value advanced by n
+    public func advancedBy(n: RawValue) -> Self? {
+       return Self(rawValue: RawValue(self.rawValue + n))
+    }
+}
