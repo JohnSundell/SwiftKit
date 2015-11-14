627d12999d0345c28ed1c5dfb6fe4a876e670d15
John Sundell <john.sundell@me.com>
2015-11-14 19:43:19 +0100

    Add CGPoint.pointOffsetByX(y:)

--- Source/Shared/CGPoint+SwiftKit.swift
+++ Source/Shared/CGPoint+SwiftKit.swift
@@ -6,4 +6,13 @@ public extension CGPoint {
     public func distanceToPoint(point: CGPoint) -> CGFloat {
         return sqrt(pow(abs(self.x - point.x), 2) + pow(abs(self.y - point.y), 2))
     }
+    
+    /// Return a new point by offsetting this point by a x and y value
+    public func pointOffsetByX(x: CGFloat, y: CGFloat) -> CGPoint {
+        var point = self
+        point.x += x
+        point.y += y
+        
+        return point
+    }
 }
