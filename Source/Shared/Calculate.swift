2e6a0b943394078f0e777d69b84fbe7435aaff0f
John Sundell <john.sundell@me.com>
2015-11-29 16:28:39 +0100

    Enable Calculate to throw

--- Source/Shared/Calculate.swift
+++ Source/Shared/Calculate.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 /// Function that can be used to encapsulate code using closures
-public func Calculate<T>(@noescape closure: () -> T) -> T {
-    return closure()
+public func Calculate<T>(@noescape closure: () throws -> T) rethrows -> T {
+    return try closure()
 }
