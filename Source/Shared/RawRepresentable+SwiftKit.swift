c942ddd009f51e6e115e7dc527dbdab27698a9b2
John Sundell <john.sundell@me.com>
2015-11-29 16:28:56 +0100

    Add convenience forEach iterator to RawRepresentable extension

--- Source/Shared/RawRepresentable+SwiftKit.swift
+++ Source/Shared/RawRepresentable+SwiftKit.swift
@@ -1,7 +1,12 @@
 import Foundation
 
-/// Extension that enables enums with a Number raw value to be strideable
-public extension RawRepresentable where RawValue: Number {
+/// SwiftKit extensions to Number-based enums
+public extension RawRepresentable where RawValue: Number, Self:Hashable {
+    /// Iterate through each member of this enum (see EnumIterator for more options)
+    public static func forEach(closure: Self -> Void) {
+        EnumIterator.iterate(closure)
+    }
+    
     /// Go to the next member of the enum
     public func next() -> Self? {
         return Self(rawValue: RawValue(self.rawValue.toDouble() + 1))
