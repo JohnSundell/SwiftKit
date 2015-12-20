a6ce11d312b234336df6e2c72eda9ec9cdf24a35
John Sundell <john.sundell@me.com>
2015-12-20 17:52:16 +0100

    NSError+SwiftKit: Use correct description key

--- Source/Shared/NSError+SwiftKit.swift
+++ Source/Shared/NSError+SwiftKit.swift
@@ -4,6 +4,6 @@ import Foundation
 public extension NSError {
     /// Create an error object with a given name and a default status code
     public convenience init(name: String) {
-        self.init(domain: name, code: 1, userInfo: [NSUnderlyingErrorKey: name])
+        self.init(domain: name, code: 1, userInfo: [NSLocalizedDescriptionKey: name])
     }
 }
