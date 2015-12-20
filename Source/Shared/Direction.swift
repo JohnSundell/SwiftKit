79eb27ba6e8255723cd875cb949346f32d75a98f
John Sundell <john.sundell@me.com>
2015-12-20 22:49:16 +0100

    Add Unbox support to Direction types

--- Source/Shared/Direction.swift
+++ Source/Shared/Direction.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 /// Protocol defining shared APIs for Direction types
-public protocol DirectionType: LoopableEnum {
+public protocol DirectionType: LoopableEnum, UnboxableByTransform {
+    /// Initialize an instance of this Direction type with a string equivalent to a member name
+    init?(string: String)
     /// Return the next clockwise direction
     func nextClockwiseDirection() -> Self
     /// Return the next counter clockwise direction
@@ -14,6 +16,8 @@ public protocol DirectionType: LoopableEnum {
 public struct Direction {
     /// An enum that describes a direction in any of 4 ways
     public enum FourWay: Int, DirectionType {
+        public typealias UnboxRawValueType = String
+
         case Up
         case Right
         case Down
@@ -22,10 +26,27 @@ public struct Direction {
         public static func lastValue() -> FourWay {
             return .Left
         }
+
+        public init?(string: String) {
+            switch string {
+            case "Up":
+                self = .Up
+            case "Right":
+                self = .Right
+            case "Down":
+                self = .Down
+            case "Left":
+                self = .Left
+            default:
+                return nil
+            }
+        }
     }
     
     /// An ennum that describes a direction in any of 8 ways
     public enum EightWay: Int, DirectionType {
+        public typealias UnboxRawValueType = String
+
         case Up
         case UpRight
         case Right
@@ -39,6 +60,29 @@ public struct Direction {
             return .LeftUp
         }
         
+        public init?(string: String) {
+            switch string {
+            case "Up":
+                self = .Up
+            case "UpRight":
+                self = .UpRight
+            case "Right":
+                self = .Right
+            case "RightDown":
+                self = .RightDown
+            case "Down":
+                self = .Down
+            case "DownLeft":
+                self = .DownLeft
+            case "Left":
+                self = .Left
+            case "LeftUp":
+                self = .LeftUp
+            default:
+                return nil
+            }
+        }
+
         public init(_ fourWayDirection: FourWay) {
             switch fourWayDirection {
             case .Up:
@@ -55,11 +99,19 @@ public struct Direction {
 }
 
 /// Default implementations of the DirectionType protocol
-public extension DirectionType where RawValue: Number {
+public extension DirectionType where RawValue: Number, UnboxRawValueType == String {
     static func firstValue() -> Self {
         return Self(rawValue: RawValue(0))!
     }
     
+    static func transformUnboxedValue(unboxedValue: String) -> Self? {
+        return self.init(string: unboxedValue)
+    }
+
+    static func unboxFallbackValue() -> Self {
+        return self.firstValue()
+    }
+
     func nextClockwiseDirection() -> Self {
         return self.next()
     }
--- Tests/DirectionTests.swift
+++ Tests/DirectionTests.swift
@@ -7,4 +7,28 @@ class DirectionTests: XCTestCase {
         XCTAssertEqual(Direction.EightWay.Left.oppositeDirection(), Direction.EightWay.Right)
         XCTAssertEqual(Direction.EightWay.RightDown.oppositeDirection(), Direction.EightWay.LeftUp)
     }
+    
+    func testUnboxSupport() {
+        struct Model: Unboxable {
+            let fourWayDirection: Direction.FourWay
+            let eightWayDirection: Direction.EightWay
+            
+            init(unboxer: Unboxer) {
+                self.fourWayDirection = unboxer.unbox("4way")
+                self.eightWayDirection = unboxer.unbox("8way")
+            }
+        }
+        
+        do {
+            let model: Model = try UnboxOrThrow([
+                "4way" : "Right",
+                "8way" : "DownLeft"
+            ])
+            
+            XCTAssertEqual(model.fourWayDirection, Direction.FourWay.Right)
+            XCTAssertEqual(model.eightWayDirection, Direction.EightWay.DownLeft)
+        } catch {
+            XCTFail("Unexpected error: \(error)")
+        }
+    }
 }
