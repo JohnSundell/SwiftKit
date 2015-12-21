8a67a388d1ac1600bf6138fb0e5fa477ada7d092
John Sundell <john.sundell@me.com>
2015-12-22 00:53:32 +0100

    Dispatch: Add Async convenience dispatch function

--- Source/Shared/Dispatch.swift
+++ Source/Shared/Dispatch.swift
@@ -12,6 +12,16 @@ public func OnMainQueue(closure: VoidClosure) {
     }
 }
 
+/// Execute a closure asynchronously on a given queue
+public func Async(queue: dispatch_queue_t, closure: VoidClosure) {
+    dispatch_async(queue, closure)
+}
+
+/// Execute a closure asynchronously on a new serial background queue with a given label
+public func Async(queueLabel: String, closure: VoidClosure) {
+    Async(dispatch_queue_create(queueLabel, nil), closure: closure)
+}
+
 /// Delay a closure by n number of seconds (and perform it on the Application's main queue)
 public func After(delay: NSTimeInterval, executeClosure closure: VoidClosure) {
     let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
