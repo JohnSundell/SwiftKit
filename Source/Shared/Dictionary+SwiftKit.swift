c1dfec6db5937fb2d8584771b3200331b3b52226
John Sundell <john.sundell@me.com>
2015-12-22 00:56:02 +0100

    Dictionary+SwiftKit: Differentiate nested collection API on Equatable

--- Source/Shared/Dictionary+SwiftKit.swift
+++ Source/Shared/Dictionary+SwiftKit.swift
@@ -3,7 +3,7 @@ import Foundation
 /// SwiftKit extensions to the standard library Dictionary
 public extension Dictionary {
     /// Transform this dictionary into another one by applying a transform on each key/value pair
-    func transform<K: Hashable, V>(transformation: (Key, Value) -> (key: K, value: V)) -> [K : V] {
+    public func transform<K: Hashable, V>(transformation: (Key, Value) -> (key: K, value: V)) -> [K : V] {
         var transformed = [K : V]()
         
         for (key, value) in self {
@@ -16,7 +16,7 @@ public extension Dictionary {
 }
 
 /// Extension to the standard library Dictionary that makes it easier to work with nested collections
-public extension Dictionary where Value: MutableIndexedCollectionType, Value: EmptyInitializable, Value.Generator.Element: Equatable {
+public extension Dictionary where Value: MutableIndexedCollectionType, Value: EmptyInitializable {
     /// Insert a value into a nested collection
     public mutating func insertValue(value: Value.Generator.Element, intoCollectionForKey key: Key) {
         var collection = self[key] ?? Value()
@@ -24,13 +24,26 @@ public extension Dictionary where Value: MutableIndexedCollectionType, Value: Em
         self[key] = collection
     }
     
-    /// Remove a value from a nested collection
-    public mutating func removeValue(value: Value.Generator.Element, atIndex index: Value.Index? = nil, fromCollectionForKey key: Key) {
+    /// Remove a value at a certain index from a nested collection
+    public mutating func removeValueAtIndex(index: Value.Index, fromCollectionForKey key: Key) {
         guard var collection = self[key] else {
             return
         }
         
-        guard let index = index ?? self[key]?.indexOf(value) else {
+        collection.removeAtIndex(index)
+        self[key] = collection
+    }
+}
+
+/// Convenience APIs for nested mutable indexed collections where the values are equatable
+public extension Dictionary where Value: MutableIndexedCollectionType, Value.Generator.Element: Equatable {
+    /// Remove a value from a nested collection by finding it
+    public mutating func removeValue(value: Value.Generator.Element, fromCollectionForKey key: Key) {
+        guard var collection = self[key] else {
+            return
+        }
+        
+        guard let index = collection.indexOf(value) else {
             return
         }
         
