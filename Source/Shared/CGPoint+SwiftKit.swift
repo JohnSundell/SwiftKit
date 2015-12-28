d05ca0d907a38c4951a9f38fe1d8a642b4e1c0d1
John Sundell <john.sundell@me.com>
2015-12-29 00:53:13 +0100

    Add CGPoint.pointMultipliedByX(y:)

--- Source/Shared/CGPoint+SwiftKit.swift
+++ Source/Shared/CGPoint+SwiftKit.swift
@@ -9,10 +9,11 @@ public extension CGPoint {
     
     /// Return a new point by offsetting this point by a x and y value
     public func pointOffsetByX(x: CGFloat, y: CGFloat) -> CGPoint {
-        var point = self
-        point.x += x
-        point.y += y
-        
-        return point
+        return CGPoint(x: self.x + x, y: self.y + y)
+    }
+    
+    /// Return a new point by multiplying this point's x and y values by a factor
+    public func pointMultipliedByX(x: CGFloat, y: CGFloat) -> CGPoint {
+        return CGPoint(x: self.x * x, y: self.y * y)
     }
 }
