04dbb6010873d9d028f56d0e7aa5f504fbb71bb3
John Sundell <john.sundell@me.com>
2015-12-29 01:13:43 +0100

    Add Scale

---
+++ Source/Shared/Scale.swift
@@ -0,0 +1,26 @@
+import Foundation
+import CoreGraphics
+
+/// Structure acting as a namespace for types describing scales
+public struct Scale<T: Number> {
+    public typealias TwoDimensional = Scale_2D<T>
+}
+
+/// Structure describing a two-dimensional scale
+public struct Scale_2D<T: Number>: Hashable, EmptyInitializable {
+    public var hashValue: Int { return "\(self.x):\(self.y)".hashValue }
+    
+    public var x: T
+    public var y: T
+    
+    /// Initialize a value, nil = 1
+    public init(x: T, y: T? = nil) {
+        self.x = x ?? T(1)
+        self.y = y ?? T(1)
+    }
+    
+    /// Initialize an empty value (scale = 1)
+    public init() {
+        self.init(x: T(1), y: T(1))
+    }
+}
