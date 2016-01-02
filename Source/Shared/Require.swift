1aba8c18a88cac8ad5886cb5cf9d1efca1d0d6ce
John Sundell <john.sundell@me.com>
2016-01-02 04:58:48 +0100

    Require: Add an optional error message parameter to all variants

--- Source/Shared/Require.swift
+++ Source/Shared/Require.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-/// Require that an optional is not nil, or throw an NSError
-public func Require<T>(optional: Optional<T>) throws -> T {
+/// Require that an optional is not nil, or throw an NSError (with an optional explicit message)
+public func Require<T>(optional: Optional<T>, errorMessage: String? = nil) throws -> T {
     guard let value = optional else {
         throw NSError(name: "Required value was nil")
     }
@@ -9,18 +9,18 @@ public func Require<T>(optional: Optional<T>) throws -> T {
     return value
 }
 
-/// Require that a value is not nil, and that it may be cast to another type, or throw an NSError
-public func RequireAndCast<A, B>(value: A?) throws -> B {
-    guard let value = try Require(value) as? B else {
-        throw NSError(name: "Could not perform cast")
+/// Require that a value is not nil, and that it may be cast to another type, or throw an NSError (with an optional explicit message)
+public func RequireAndCast<A, B>(value: A?, errorMessage: String? = nil) throws -> B {
+    guard let value = try Require(value, errorMessage: errorMessage) as? B else {
+        throw NSError(name: errorMessage ?? "Could not perform cast")
     }
     
     return value
 }
 
-/// Require that an expression avaluates to true, or throw an NSError
-public func Require(@autoclosure expression: () -> Bool) throws {
+/// Require that an expression avaluates to true, or throw an NSError (with an optional explicit message)
+public func Require(@autoclosure expression: () -> Bool, errorMessage: String? = nil) throws {
     if !expression() {
-        throw NSError(name: "Requirement not fulfilled")
+        throw NSError(name: errorMessage ?? "Requirement not fulfilled")
     }
 }
