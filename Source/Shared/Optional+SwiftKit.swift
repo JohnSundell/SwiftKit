963bee086b8702f389be43c5bfc1042b4c90e475
John Sundell <john.sundell@me.com>
2016-01-07 23:48:38 +0100

    Optional+SwiftKit: Enable .require() to take a custom error message

--- Source/Shared/Optional+SwiftKit.swift
+++ Source/Shared/Optional+SwiftKit.swift
@@ -3,7 +3,7 @@ import Foundation
 /// SwiftKit extensions to Swift's Optional type
 public extension Optional {
     /// Unwrap this optional's value, or throw an NSError. See `Require()` for more info.
-    public func require() throws -> Wrapped {
-        return try Require(self)
+    public func require(errorMessage: String? = nil) throws -> Wrapped {
+        return try Require(self, errorMessage: errorMessage)
     }
 }
