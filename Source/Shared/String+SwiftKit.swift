42adfbc54c313a0223886dec1c2260fa6458e9cb
John Sundell <john.sundell@me.com>
2016-01-07 23:48:23 +0100

    String: Conform to StringConvertible

--- Source/Shared/String+SwiftKit.swift
+++ Source/Shared/String+SwiftKit.swift
@@ -5,3 +5,10 @@ public extension String {
     /// The length of the string as an Int
     public var length: Int { return self.characters.count }
 }
+
+/// Make String a StringConvertible type that returns itself
+extension String: StringConvertible {
+    public func toString() -> String {
+        return self
+    }
+}
