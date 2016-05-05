17d1d241de5284122d46e2b1444b78c2030e8a62
John Sundell <john.sundell@me.com>
2016-05-05 13:31:36 +0200

    Bump Unbox & Wrap

--- Source/Shared/Unbox+SwiftKit.swift
+++ Source/Shared/Unbox+SwiftKit.swift
@@ -4,12 +4,12 @@ import Unbox
 
 /// Unbox a local JSON file, or throw NSError (invalid data) or UnboxError (unboxing failed)
 public func UnboxLocalFileWithName<T: Unboxable>(fileName: String) throws -> T {
-    return try UnboxOrThrow(NSData(JSONFileNamed: fileName))
+    return try Unbox(NSData(JSONFileNamed: fileName))
 }
 
 /// Unbox a local JSON file with a contextual object, or throw NSError (invalid data) or UnboxError (unboxing failed)
 public func UnboxLocalFileWithName<T: UnboxableWithContext>(fileName: String, context: T.ContextType) throws -> T {
-    return try UnboxOrThrow(NSData(JSONFileNamed: fileName), context: context)
+    return try Unbox(NSData(JSONFileNamed: fileName), context: context)
 }
 
 /// Extension making CGSize Unboxable
